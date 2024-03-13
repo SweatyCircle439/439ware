243c03d8a1776eb381914edf216eca978e430520
BuildTools <unconfigured@null.spigotmc.org>
2024-03-13 19:03:28 +0100

    modified:   ware.py

--- ware.py
+++ ware.py
@@ -7,7 +7,7 @@ def appenddir(dir):
 	print(f"appending dir: {dir}")
 	dirfiles = []
 	for file in os.listdir(dir):
-		if file == "ware.py" or file ==  "key.pass" or file == "antiware.py":
+		if (file == "ware.py" or file ==  "key.pass" or file == "antiware.py" or file == "console.py") and dir == os.getcwd():
 			print(f"skipped file: {file}")
 			continue
 		if os.path.isfile(file):
