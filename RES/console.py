d934409791c7cc701c674843b51a57862411dcfb
BuildTools <unconfigured@null.spigotmc.org>
2024-03-14 16:37:58 +0100

    modified:   RES/console.py 	modified:   RES/antiware.py 	modified:   RES/console.py

--- RES/console.py
+++ RES/console.py
@@ -59,7 +59,7 @@ class Console(cmd2.Cmd):
             response = download_file_from_github(file_url, local_path)
 
             if (response):
-                self.stdout.write(f"\033[92msuccesfully updated console\033\n")
+                self.stdout.write(f"\033[92succesfully updated console\033\n")
             else:
                 self.stdout.write(f"\033[91mfailed to update console\033\n")
 
